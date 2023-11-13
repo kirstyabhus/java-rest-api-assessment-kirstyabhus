134ae80aad82d4d985e8689c5c5739aae636f54c
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-13 11:44:09 +0000

    add portfolio field to investments

--- src/main/java/com/cbfacademy/apiassessment/model/ETF.java
+++ src/main/java/com/cbfacademy/apiassessment/model/ETF.java
@@ -3,12 +3,24 @@ package com.cbfacademy.apiassessment.model;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class ETF implements Investment {
     private UUID id;
     private String symbol;
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "potfolio_id")
+    private Portfolio portfolio; // ?
+
+    public ETF(UUID id, String symbol, String name, Portfolio portfolio) {
+        this.id = UUID.randomUUID();
+        this.symbol = symbol;
+        this.name = name;
+    }
 
     public UUID getId() {
         return id;
@@ -27,6 +39,14 @@ public class ETF implements Investment {
     }
 
     public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Portfolio getPortfolio() {
         return symbol;
     }
 
--- src/main/java/com/cbfacademy/apiassessment/model/Portfolio.java
+++ src/main/java/com/cbfacademy/apiassessment/model/Portfolio.java
@@ -4,19 +4,18 @@ import java.util.Map;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Portfolio {
 
     private UUID id;
     private String name;
-    private String description; // might get rid of
     private Map<String, Investment> investments;
 
-    public Portfolio(UUID id, String name, String description, Map<String, Investment> investments) {
+    public Portfolio(UUID id, String name, Map<String, Investment> investments) {
         this.id = UUID.randomUUID();
         this.name = name;
-        this.description = description;
         this.investments = investments;
     }
 
@@ -30,14 +29,6 @@ public class Portfolio {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Map<String, Investment> getInvestments() {
