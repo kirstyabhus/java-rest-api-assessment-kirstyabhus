41525abbf336f7268566b45fce325d74e99e9dcc
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-13 12:09:39 +0000

    create InvestmentRepository

---
+++ src/main/java/com/cbfacademy/apiassessment/repository/InvestmentRepository.java
@@ -0,0 +1,11 @@
+package com.cbfacademy.apiassessment.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cbfacademy.apiassessment.model.Investment;
+
+public interface InvestmentRepository extends JpaRepository<Investment, UUID> {
+
+}
