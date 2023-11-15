2ad81702812caeca60f5dc9b335a66cf6dae3fef
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-15 14:44:23 +0000

    add GET portfolioById

--- src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
@@ -1,9 +1,11 @@
 package com.cbfacademy.apiassessment.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class PortfolioController {
     @GetMapping("")
     public List<Portfolio> getAllPortfolios() {
         return repository.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Portfolio getPortfolioById(@PathVariable UUID id) {
+        return repository.findById(id);
     }
 
 }
