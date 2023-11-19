513513143713bfd4369d12ece0a33debfc4ec181
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-19 20:14:03 +0000

    Revert "final"
    This reverts commit 05f2116b5fdb572ec06403ea7af18018848bd8f6.

--- src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cbfacademy.apiassessment.model.Investment;
 import com.cbfacademy.apiassessment.model.Portfolio;
 import com.cbfacademy.apiassessment.service.PortfolioService;
 
