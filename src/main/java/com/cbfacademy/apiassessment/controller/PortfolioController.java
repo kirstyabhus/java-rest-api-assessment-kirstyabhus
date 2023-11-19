05f2116b5fdb572ec06403ea7af18018848bd8f6
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-19 18:49:38 +0000

    final

--- src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/PortfolioController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cbfacademy.apiassessment.model.Investment;
 import com.cbfacademy.apiassessment.model.Portfolio;
 import com.cbfacademy.apiassessment.service.PortfolioService;
 
