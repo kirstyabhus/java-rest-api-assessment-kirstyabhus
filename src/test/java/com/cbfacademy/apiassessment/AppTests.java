f11fab03862deae1433bea2c9a03f10096058bdd
Kirsty Abhulimhen <kabhulimhen@gmail.com>
2023-11-19 22:29:11 +0000

    add tests

--- src/test/java/com/cbfacademy/apiassessment/AppTests.java
+++ src/test/java/com/cbfacademy/apiassessment/AppTests.java
@@ -7,13 +7,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Description;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.cbfacademy.apiassessment.model.Investment;
+import com.cbfacademy.apiassessment.model.Portfolio;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = Test.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = PortfolioManagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class AppTests {
 
 	@LocalServerPort
@@ -26,24 +39,36 @@ class AppTests {
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		this.base = new URL("http://localhost:" + port + "/greeting");
+		this.base = new URL("http://localhost:" + port + "/api/v1/portfolios");
 	}
 
 	@Test
-	@Description("/greeting endpoint returns expected response for default name")
-	public void greeting_ExpectedResponseWithDefaultName() {
-		ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
-
+	@Description("/api/v1/portfolios endpoint returns all portfolios")
+	public void getAllPortfoliosReturnsPortfoliosList() {
+		ResponseEntity<Portfolio[]> response = restTemplate.getForEntity(base.toString(), Portfolio[].class);
 		assertEquals(200, response.getStatusCode().value());
-		assertEquals("Hello World", response.getBody());
+		assertTrue(Objects.requireNonNull(response.getBody()).length > 0);
 	}
 
 	@Test
-	@Description("/greeting endpoint returns expected response for specified name parameter")
-	public void greeting_ExpectedResponseWithNameParam() {
-		ResponseEntity<String> response = restTemplate.getForEntity(base.toString() + "?name=John", String.class);
+	@Description("/api/v1/portfolios/{id} endpoint returns portfolio by ID")
+	public void getPortfolioByIdReturnsPortfolio() {
+		ResponseEntity<List<Portfolio>> responseList = restTemplate.exchange(
+				base.toString(),
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<>() {
+				});
+		UUID existingId = Objects.requireNonNull(responseList.getBody()).get(0).getPortfolioId();
 
-		assertEquals(200, response.getStatusCode().value());
-		assertEquals("Hello John", response.getBody());
+		ResponseEntity<Portfolio> response = restTemplate.getForEntity(base.toString() + "/" + existingId,
+				Portfolio.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+
+		// If the portfolio is found, check its attributes or structure as needed
+		Portfolio portfolio = response.getBody();
+		assertNotNull(portfolio);
+		assertEquals(existingId, portfolio.getPortfolioId());
 	}
+
 }
